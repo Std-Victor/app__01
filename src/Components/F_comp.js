59120cff3b1dc6589ac56adcc7f510c2b230cc76
Std-Victor <smallvictor220@gmail.com>
2022-09-18 19:25:52 +0100

    add app__01

---
+++ src/Components/F_comp.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+function FComp() {
+    const [notes, setNotes] = useState([{DataBase:19}, {ReactJs : 18}])
+    const data = notes.map(n => Object.entries(n))
+    return (
+      <div>
+        <ul>
+          {data.map(([[k,v]])=> <li key={k}>{k} : {v}</li>)}
+        </ul>
+        <ul>
+          {notes.map(note => Object.entries(note).map(([k,v])=> <li key={k}>{k}:{v}</li>))}
+        </ul>
+      </div>
+    );
+}
+
+export default FComp;
